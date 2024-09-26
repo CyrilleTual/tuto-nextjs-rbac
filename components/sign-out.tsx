0d8ab6d0767ab3fba6af6df62a15c87d0a5af143
cyrilletual <cyrille.tual@gmail.com>
2024-09-26 10:59:42 +0200

    signOut-client-component

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -1,21 +1,19 @@
 /* components/sign-out.tsx */
 
-import { signOut } from "@/auth";
+"use client";
 
+import { signOut } from "next-auth/react";
 export function SignOut() {
+
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signOut();
+    <button
+      onClick={() => {
+       signOut();
       }}
+      type="submit"
+      className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition duration-300"
     >
-      <button
-        type="submit"
-        className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition duration-300"
-      >
-        Sign out
-      </button>
-    </form>
+      Sign out
+    </button>
   );
 }
